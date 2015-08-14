a29644cb678e8c517f3157109c504d9728716862
Simon Poirier <simpoir@gmail.com>
2015-08-14 09:09:56 -0400

    bump version

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,8 +1,8 @@
 Pebble.addEventListener('showConfiguration', function(e) {
   console.log('configuring');
   //Pebble.openURL('http://similipoulet:8000/config.html');
-  //Pebble.openURL('https://cdn.rawgit.com/simpoir/pebble-chromacyle/v1.1/src/js/config.html');
-  Pebble.openURL('https://rawgit.com/simpoir/pebble-chromacyle/master/src/js/config.html');
+  Pebble.openURL('https://cdn.rawgit.com/simpoir/pebble-chromacyle/v1.1/src/js/config.html');
+  //Pebble.openURL('https://rawgit.com/simpoir/pebble-chromacyle/master/src/js/config.html');
 });
 
 Pebble.addEventListener('webviewclosed', function(e) {
